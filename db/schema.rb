6d7e6fb64bc2842275e85cd859616c741fa3ca33
Carolina Brito <carol.o.ag@gmail.com>
2020-12-19 12:40:10 -0300

    Generate and run the new migration for add user id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_16_213740) do
+ActiveRecord::Schema.define(version: 2020_12_19_152211) do
 
   create_table "exams", force: :cascade do |t|
     t.string "date"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 2020_12_16_213740) do
     t.string "cholesterol"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_exams_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
